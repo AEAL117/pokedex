bea3b26b718ba2d46ba0a82d396717017348c0e4
AEAL117 <61563639+AEAL117@users.noreply.github.com>
2021-01-14 14:03:59 -0600

    Alfredo
    Se agrego PokemonRepositoryPersistence y sus metodos asi como PokemonCrudRepository y sus metodos

---
+++ src/main/java/com/acepokedex/pokedex/persistence/PokemonRepositoryPersistence.java
@@ -0,0 +1,52 @@
+package com.acepokedex.pokedex.persistence;
+
+import com.acepokedex.pokedex.domain.Pokemon;
+import com.acepokedex.pokedex.domain.repository.PokemonRepository;
+import com.acepokedex.pokedex.persistence.crud.PokemonCrudRepository;
+import com.acepokedex.pokedex.persistence.entity.EntityPokemon;
+import com.acepokedex.pokedex.persistence.mapper.PokemonMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class PokemonRepositoryPersistence implements PokemonRepository {
+    @Autowired
+    private PokemonCrudRepository pokemonCrudRepository;
+    @Autowired
+    private PokemonMapper pokemonMapper;
+
+    @Override
+    public List<Pokemon> getAll() {
+        List<EntityPokemon> pokemons = (List<EntityPokemon>) pokemonCrudRepository.findAll();
+        return pokemonMapper.toPokemons(pokemons);
+    }
+
+    @Override
+    public Optional<Pokemon> getPokemonByName(String name) {
+
+        return pokemonCrudRepository
+                .findPokemonByName(name)
+                .map(entityPokemons -> pokemonMapper
+                        .toPokemon((EntityPokemon) entityPokemons));
+    }
+
+    @Override
+    public Optional<Pokemon> getPokemonById(int id) {
+        return pokemonCrudRepository
+                .findPokemonById(id).map(pokemon -> pokemonMapper.toPokemon((EntityPokemon) pokemon));
+    }
+
+    @Override
+    public Pokemon save(Pokemon pokemon) {
+        EntityPokemon entityPokemon = pokemonMapper.toEntityPokemon(pokemon);
+        return pokemonMapper.toPokemon(pokemonCrudRepository.save(entityPokemon));
+    }
+
+    @Override
+    public void delete(int idPokemon) {
+        pokemonCrudRepository.deleteById(idPokemon);
+    }
+}
--- src/main/java/com/acepokedex/pokedex/persistence/crud/PokemonCrudRepository.java
+++ src/main/java/com/acepokedex/pokedex/persistence/crud/PokemonCrudRepository.java
@@ -3,6 +3,10 @@ package com.acepokedex.pokedex.persistence.crud;
 import com.acepokedex.pokedex.persistence.entity.EntityPokemon;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PokemonCrudRepository extends CrudRepository<EntityPokemon,Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface PokemonCrudRepository extends CrudRepository<EntityPokemon,Integer> {
+Optional<List<EntityPokemon>> findPokemonById(int id);
+Optional<List<EntityPokemon>> findPokemonByName(String name);
 }
